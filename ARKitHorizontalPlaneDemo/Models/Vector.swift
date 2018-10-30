558d67a5f5c0fa8153f3eca67f3ac170c56e6ee5
Dagmawi Nadew <techoffen4@gmail.com>
2018-10-30 06:12:19 -0400

    Refactor

---
+++ ARKitHorizontalPlaneDemo/Models/Vector.swift
@@ -0,0 +1,23 @@
+//
+//  Vector.swift
+//  ARKitHorizontalPlaneDemo
+//
+//  Created by Dagmawi Nadew-Assefa on 10/30/18.
+//  Copyright © 2018 eye_Ohh_ess. All rights reserved.
+//
+
+import Foundation
+import simd
+
+struct Vector {
+    
+    let xCoordinate: Float
+    let yCoordinate: Float
+    let zCoordinate: Float
+    
+    init(from translation: float3) {
+        self.xCoordinate = translation.x
+        self.yCoordinate = translation.y
+        self.zCoordinate = translation.z
+    }
+}
